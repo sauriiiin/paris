e6fefa3740177563a8d01d5544512df5ac125884
saur1n <saur1n@users.noreply.github.com>
2019-03-29 13:06:46 -0400

    update

--- techPowA.m
+++ techPowA.m
@@ -43,7 +43,8 @@
 
         temp_norm      = sprintf('%s_TEMP_%d_NORM',expt_name,density);
         temp_fit       = sprintf('%s_TEMP_%d_FITNESS',expt_name,density);
-        temp_pval       = sprintf('%s_TEMP_%d_PVALUE',expt_name,density);
+        temp_fits      = sprintf('%s_TEMP_%d_FITNESS_STATS',expt_name,density);
+        temp_pval      = sprintf('%s_TEMP_%d_PVALUE',expt_name,density);
     %   Reference Strain Name
 
         cont.name           = 'BF_control';
@@ -76,7 +77,9 @@
         
         cdata = [];
 
-        for t = 2:length(hours)
+        for t = 5:length(hours)
+%%  GENERATE FITNESS DATA
+
             exec(conn, sprintf('drop table %s',temp_norm));
             exec(conn, sprintf(['create table %s ( ',...
                 'pos int(11) not NULL, ',...
@@ -161,60 +164,144 @@
             end
             datainsert(conn, temp_norm,...
                     {'pos','hours','bg','average','fitness'},fdata);
+                
             exec(conn, sprintf('drop table %s',temp_fit)); 
             exec(conn, sprintf(['create table %s ',...
                 '(select b.orf_name, a.pos, a.hours, a.bg, a.average, a.fitness ',...
                 'from %s a, %s b ',...
                 'where a.pos = b.pos ',...
                 'order by a.pos asc)'],temp_fit,temp_norm,tablename_p2o));
-            
-            
-            
-            
+%%  FITNESS STATS            
+            exec(conn, sprintf('drop table %s', temp_fits));
+            exec(conn, sprintf(['create table %s (orf_name varchar(255) null, ',...
+                'hours int not null, N int not null, cs_mean double null, ',...
+                'cs_median double null, cs_std double null)'],temp_fits));
 
-            ef_size = mean(rest_fit)/mean(cont_fit);
+            colnames_fits = {'orf_name','hours','N','cs_mean','cs_median','cs_std'};
 
-            all = length(rest_means);
-            pp = sum(temp_p<0.05);
-            pow = (pp/all)*100;
+            stat_data = fit_stats(temp_fit);
+            tic
+            datainsert(conn,temp_fits,colnames_fits,stat_data)
+            toc
+%%  STATS TO PVALUES            
+            exec(conn, sprintf('drop table %s',temp_pval));
+            exec(conn, sprintf(['create table %s (orf_name varchar(255) null,'...
+                'hours int not null, p double null, stat double null)'],temp_pval));
+            colnames_pval = {'orf_name','hours','p','stat'};
 
-            cdata = [cdata; ef_size, pow];
-            data = [data; ef_size, pow];
+            for iii = 1:length(rest_hrs)
+                contpos = fetch(conn, sprintf(['select pos from %s ',...
+                    'where orf_name = ''%s'' and pos < 10000'],...
+                    tablename_p2o,cont.name));
+                contpos = contpos.pos + [110000,120000,130000,140000,...
+                    210000,220000,230000,240000];
+
+                contfit = [];
+                for ii = 1:length(contpos)
+                    temp = fetch(conn, sprintf(['select fitness from %s ',...
+                        'where hours = %d and pos in (%s) ',...
+                        'and fitness is not null'],temp_fit,rest_hrs(iii),...
+                        sprintf('%d,%d,%d,%d,%d,%d,%d,%d',contpos(ii,:))));
+                    if nansum(temp.fitness) > 0
+                        contfit = [contfit, nanmean(temp.fitness)];
+                    end
+                end
+
+                contmean = nanmean(contfit);
+                contstd = nanstd(contfit);
+
+                orffit = fetch(conn, sprintf(['select orf_name, cs_median, ',...
+                    'cs_mean, cs_std from %s ',...
+                    'where hours = %d and orf_name != ''%s'' ',...
+                    'order by orf_name asc'],temp_fits,rest_hrs(iii),cont.name));
+
+                m = contfit';
+                tt = length(m);
+
+                pvals = [];
+                stat = [];
+                for i = 1:length(orffit.orf_name)
+                    if sum(m<orffit.cs_mean(i)) < tt/2
+                        if m<orffit.cs_mean(i) == 0
+                            pvals = [pvals; 1/tt];
+                            stat = [stat; (orffit.cs_mean(i) - contmean)/contstd];
+                        else
+                            pvals = [pvals; ((sum(m<=orffit.cs_mean(i)))/tt)*2];
+                            stat = [stat; (orffit.cs_mean(i) - contmean)/contstd];
+                        end
+                    else
+                        pvals = [pvals; ((sum(m>=orffit.cs_mean(i)))/tt)*2];
+                        stat = [stat; (orffit.cs_mean(i) - contmean)/contstd];
+                    end
+                end
+
+                pdata{iii}.orf_name = orffit.orf_name;
+                pdata{iii}.hours = ones(length(pdata{iii}.orf_name),1)*rest_hrs(iii);
+                pdata{iii}.p = num2cell(pvals);
+                pdata{iii}.p(cellfun(@isnan,pdata{iii}.p)) = {[]};
+                pdata{iii}.stat = num2cell(stat);
+                pdata{iii}.stat(cellfun(@isnan,pdata{iii}.stat)) = {[]};
+
+                datainsert(conn,temp_pval,colnames_pval,pdata{iii})
+            end
+%%  CALCULATE POWER & ES
+            cfit = fetch(conn, sprintf(['select fitness from %s ',...
+                'where hours = %d and orf_name = ''%s'' ',...
+                'and fitness is not NULL'],...
+                temp_fit,cont_hrs,cont.name));
+            cont_fit = cfit.fitness;
+
+            for iii = 1:length(rest_hrs)
+                rfit = fetch(conn, sprintf(['select fitness from %s ',...
+                    'where hours = %d and orf_name != ''%s'' ',...
+                    'and fitness is not NULL'],...
+                    temp_fit,rest_hrs(iii),cont.name));
+                rest_fit = rfit.fitness;
+                ef_size = mean(rest_fit)/mean(cont_fit);
+                
+                pp = fetch(conn, sprintf(['select p from %s ',...
+                    'where hours = %d '],...
+                    temp_pval,rest_hrs(iii)));
+                
+                pow = (sum(pp.p<=0.05)/length(pp.p))*100;
+                cdata = [cdata; ef_size, pow];
+                data = [data; ef_size, pow]; 
+            end
 
         %%  POWER vs ES
-% 
-%                 [~, i] = sort(data(:,1));
-%                 es_pow = data(i, :);
-% 
-%                 x{cont_hrs}{ss}   = es_pow(:,1);
-%                 y{cont_hrs}{ss}   = es_pow(:,2);
-%                 xx{cont_hrs}{ss}  = min(es_pow(:,1)):.001:max(es_pow(:,1));
-%                 yy{cont_hrs}{ss}  = interp1(x{cont_hrs}{ss},y{cont_hrs}{ss},...
-%                     xx{cont_hrs}{ss},'pchip');
-% 
-%         %         figure()
-%                 fig = figure('Renderer', 'painters', 'Position', [10 10 960 800],'visible','off');
-%                 plot(xx{cont_hrs}{ss},yy{cont_hrs}{ss},'Color',[0.5 0.75 1],'LineWidth',2)
-%                 grid on
-%                 grid minor
-%                 xlim([0.6,1.4])
-%                 ylim([-1,101])
-%                 xlabel('Effect Size (Relative Fitness)')
-%                 ylabel('Power')
-%                 hold on
-%                 scatter(x{cont_hrs}{ss}, y{cont_hrs}{ss},'MarkerEdgeColor',[0 .5 .5],...
-%                           'MarkerFaceColor',[0 .7 .7],...
-%                           'LineWidth',2);
-%                 hold on
-%                 title(sprintf('ES V/S Power\nTime = %dhrs | SS = %d | FPR = %.2f%%',cont_hrs, ss, fpr))
-%                 hold off
-%                 saveas(fig,sprintf('%s%s_powES_%d_%d.png',...
-%                     out_path,expt_name,cont_hrs,ss))
+
+            [~, i] = sort(data(:,1));
+            es_pow = data(i, :);
+
+            x   = es_pow(:,1);
+            y   = es_pow(:,2);
+            xx  = min(es_pow(:,1)):.001:max(es_pow(:,1));
+            yy  = interp1(x,y,...
+                xx,'pchip');
+
+    %         figure()
+%             fig = figure('Renderer', 'painters', 'Position', [10 10 960 800],'visible','off');
+            plot(xx,yy,'Color',[0.5 0.75 1],'LineWidth',2)
+            grid on
+            grid minor
+            xlim([0.6,1.4])
+            ylim([-1,101])
+            xlabel('Effect Size (Relative Fitness)')
+            ylabel('Power')
+            hold on
+            scatter(x, y,'MarkerEdgeColor',[0 .5 .5],...
+                      'MarkerFaceColor',[0 .7 .7],...
+                      'LineWidth',2);
+            hold on
+            title(sprintf('ES V/S Power\nTime = %dhrs | SS = %d | FPR = %.2f%%',cont_hrs, ss, fpr))
+            hold off
+            saveas(fig,sprintf('%s%s_TpowES_%d_%d.png',...
+                out_path,expt_name,cont_hrs,ss))
                     
-            fprintf('powAnalysis for %s at %d hrs is done.\n',...
+            fprintf('techPowA for %s at %d hrs is done.\n',...
                 expt_name,cont_hrs)
-            send_message(4124992194,'fi','powAnalysis Update',...
-                sprintf('powAnalysis for %s at %d hrs is done.\n',...
+            send_message(4124992194,'fi','techPowA Update',...
+                sprintf('techPowA for %s at %d hrs is done.\n',...
                     expt_name,cont_hrs))
         end
 %%  COMPOSITE ES AND POW RELATIONSHIP
@@ -243,15 +330,15 @@
         hold on
         title(sprintf('%s\nES V/S Power',expt))
         hold off
-        saveas(fig,sprintf('%s%s_powES_%d.png',out_path,expt_name,ss))
+        saveas(fig,sprintf('%s%s_TpowES_%d.png',out_path,expt_name,ss))
 
-        fprintf("Power V/S Effect Size Analysis For %s Complete!\n",expt_name);
-        send_message(4124992194,'fi','powAnalysis Complete',...
-            sprintf("Power V/S Effect Size Analysis For %s Complete!",expt_name))
+        fprintf("TechRep Based Power V/S Effect Size Analysis For %s Complete!\n",expt_name);
+        send_message(4124992194,'fi','techPowA Complete',...
+            sprintf("TechRep Based Power V/S Effect Size Analysis For %s Complete!",expt_name))
 
     catch me
 
         warning(me.message)
-        send_message(4124992194,'fi','powAnalysis Error',me.message)
+        send_message(4124992194,'fi','techPowA Error',me.message)
 
     end   
